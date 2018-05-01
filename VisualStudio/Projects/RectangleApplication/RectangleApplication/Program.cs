1ee3fb071274e4252f3664de6eaa02be8a41f6ed
Jaspal Dhaliwal <jasdhali@gmail.com>
2018-04-30 22:20:42 -0500

    Initial commit for Visual Studio

---
+++ VisualStudio/Projects/RectangleApplication/RectangleApplication/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleApplication
+{
+    class Rectangle
+    {
+        //members
+
+        double width;
+        double length;
+        //
+        public void AcceptDetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+        public double GetArea() { return width * length; }
+        public void Display()
+        {
+            Console.WriteLine(" Length : {0} " , length);
+            Console.WriteLine(" Width : {0} " , width );
+            Console.WriteLine(" Area : {0} "  , GetArea() );
+
+        }
+    }
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.AcceptDetails();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
